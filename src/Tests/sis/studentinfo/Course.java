7efdeb4e47d00c664b296543685fc9b0bb9611a5
GaldiLuna <fredericolluna@gmail.com>
2025-06-05 11:47:37 -0300

    evolução na leitura do Agile Java, reduzida hoje por conta de configuração do GitLab

--- src/Tests/sis/studentinfo/Course.java
+++ src/Tests/sis/studentinfo/Course.java
@@ -19,6 +19,12 @@ public class Course {
 
     @Override
     public boolean equals(Object object) {
+        if (object == null)
+            return false;
+        if (this.getClass() != object.getClass())
+            return false;
+        if (!(object instanceof Course)) //Outra forma de escrever o if logo acima
+            return false;
         Course that = (Course)object;
         return
             this.department.equals(that.department)
--- src/Tests/sis/studentinfo/CourseTest.java
+++ src/Tests/sis/studentinfo/CourseTest.java
@@ -1,5 +1,5 @@
 package Tests.sis.studentinfo;
-
+import Tests.sis.studentinfo.Course;
 import junit.framework.TestCase;
 
 public class CourseTest extends TestCase {
@@ -17,5 +17,35 @@ public class CourseTest extends TestCase {
             fail();
         Course courseB = new Course("ARTH", "330");
         assertFalse(courseA.equals(courseB));
+
+        //Sobre o contrato de Igualdade vamos ver as 5 opções de comparação abaixo:
+        //REFLEXIVIDADE
+        assertEquals(courseA, courseA);
+
+        //TRANSITIVIDADE
+        Course courseAPrime2 = new Course("NURS", "201");
+        assertEquals(courseAPrime, courseAPrime2);
+        assertEquals(courseA, courseAPrime2);
+
+        //SIMETRIA
+        assertEquals(courseAPrime, courseA);
+
+        //CONSISTÊNCIA
+        assertEquals(courseA, courseAPrime);
+
+        //COMPARAÇÃO COM NULO
+        assertFalse(courseA.equals(null));
+        assertTrue(!courseA.equals(null));
+
+        //MAÇÃS E LARANJAS
+        assertFalse(courseA.equals("CMSC-120"));
+
+        //OUTRAS COMPARAÇÕES MAIS EXPLÍCITAS
+        assertTrue(courseA.equals(courseAPrime)); // Verifica se os objetos são IGUAIS (comparando o conteúdo)
+        assertFalse(courseA == courseAPrime); // Verifica se os objetos NÃO SÃO IDÊNTICOS (não são o mesmo objeto na memória)
+        assertNotSame(courseA, courseAPrime); // O JUnit tem um metodo específico para verificar se NÃO são idênticos, que é mais claro
+        // Para demonstrar o assertSame, você pode usá-lo para verificar a reflexividade
+        // (um objeto é sempre idêntico a si mesmo)
+        assertSame(courseA, courseA);
     }
 }
